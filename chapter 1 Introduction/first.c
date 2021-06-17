235d37f6fd397c4d6a2a52b51b00fe35628bcfa6
AryanKuAg <alemantrixaryan@gmail.com>
2021-06-17 17:00:45 +0545

    fear to lose

---
+++ chapter 1 Introduction/first.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+
+// only _ is allowed in variables name from all other special characters.
+// Constant Characters must be enclosed in single inverted commas ('')
+
+long long int main(){
+    int a = 4;
+    float b = 5.3;
+    char c = 'c';
+    printf("Hello world %d %f %c \n", a, b, c);
+    printf("%f ", a + b);
+    return 0;
+}
